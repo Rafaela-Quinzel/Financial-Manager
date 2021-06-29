9e764bbc12dcbcab20317831885d7bf764a882c9
Rafaela-Quinzel <71155947+Rafaela-Quinzel@users.noreply.github.com>
2021-06-29 17:59:48 -0300

    Adicionando alterações

--- src/app/pages/categories/category-form/category-form.component.ts
+++ src/app/pages/categories/category-form/category-form.component.ts
@@ -7,7 +7,7 @@ import { CategoryService } from '../shared/category.service';
 
 import { switchMap } from 'rxjs/operators';
 
-//import toastr from "toastr";
+import * as toastr from "toastr";
 
 @Component({
   selector: 'app-category-form',
@@ -19,7 +19,7 @@ export class CategoryFormComponent implements OnInit, AfterContentChecked {
   currentAction!: string;
   categoryForm!: FormGroup;
   pageTitle!: string;
-  serverErrorMessages!: string[];
+  serverErrorMessages!: string[] | null;
   submittingForm: boolean = false;
   category: Category = new Category();
 
@@ -39,6 +39,18 @@ export class CategoryFormComponent implements OnInit, AfterContentChecked {
   ngAfterContentChecked() {
     this.setPageTitle();
   }
+
+  submitForm() {
+    this.submittingForm = true;
+
+    if (this.currentAction == "new") {
+      this.createCategory();
+
+    } else {
+      this.updateCategory();
+    }
+  }
+
 
   // PRIVATE METHODS
   private setCurrentAction() {
@@ -72,14 +84,54 @@ export class CategoryFormComponent implements OnInit, AfterContentChecked {
     }
   }
 
-
   private setPageTitle() {
     if (this.currentAction == "new") {
-      this.pageTitle = "cadstro de Nova Categoria"
-      
+      this.pageTitle = "Cadastro de Nova Categoria"
+
     } else {
       const categoryName = this.category.name || ""
       this.pageTitle = "Editando Categoria: " + categoryName;
+    }
+  }
+
+  private createCategory() {
+    const category: Category = Object.assign(new Category(), this.categoryForm.value);
+
+    this.categoryService.create(category)
+      .subscribe(
+        category => this.actionsForSuccess(category),
+        error => this.actionsForError(error)
+      )
+  }
+
+  private updateCategory() {
+    const category: Category = Object.assign(new Category(), this.categoryForm.value);
+
+    this.categoryService.update(category)
+      .subscribe(
+        category => this.actionsForSuccess(category),
+        error => this.actionsForError(error)
+      )
+  }
+
+  private actionsForSuccess(category: Category) {
+    toastr.success("Solicitação processada com sucesso!");
+
+    // redirect/reload component page.
+    this.router.navigateByUrl("categories", { skipLocationChange: true }).then(
+      () => this.router.navigate(["categories", category.id, "edit"])
+    );
+  }
+
+  private actionsForError(error: any) {
+    toastr.error("Ocorreu um erro ao processar a sua solicitação!");
+
+    this.submittingForm = false;
+
+    if (error.status === 422) {
+      this.serverErrorMessages = JSON.parse(error._body).errors;
+    } else {
+      this.serverErrorMessages = ["Falha na comunicação com o servidor. Por favor, tente mais tarde."]
     }
   }
 
--- src/app/pages/entries/shared/entry.service.ts
+++ src/app/pages/entries/shared/entry.service.ts
@@ -4,47 +4,47 @@ import { HttpClient, HttpHeaders } from "@angular/common/http";
 import { Observable, throwError } from "rxjs";
 import { map, catchError, flatMap } from "rxjs/operators";
 
-import { Category } from "./category.model";
+import { Entry } from "./entry.model";
 
 
 @Injectable({
   providedIn: 'root'
 })
-export class CategoryService {
+export class EntryService {
 
-  private apiPath: string = "api/categories";
+  private apiPath: string = "api/entries";
 
   constructor(private http: HttpClient) { }
 
-  getAll(): Observable<Category[]> {
+  getAll(): Observable<Entry[]> {
     return this.http.get(this.apiPath).pipe(
       catchError(this.handleError),
-      map(this.jsonDataToCategories)
+      map(this.jsonDataToEntries)
     )
   }
 
-  getById(id: number): Observable<Category> {
+  getById(id: number): Observable<Entry> {
     const url = `${this.apiPath}/${id}`;
 
     return this.http.get(url).pipe(
       catchError(this.handleError),
-      map(this.jsonDataToCategory)
+      map(this.jsonDataToEntry)
     )
   }
 
-  create(category: Category): Observable<Category> {
-    return this.http.post(this.apiPath, category).pipe(
+  create(entries: Entry): Observable<Entry> {
+    return this.http.post(this.apiPath, entries).pipe(
       catchError(this.handleError),
-      map(this.jsonDataToCategory)
+      map(this.jsonDataToEntry)
     )
   }
 
-  update(category: Category): Observable<Category> {
-    const url = `${this.apiPath}/${category.id}`;
+  update(entries: Entry): Observable<Entry> {
+    const url = `${this.apiPath}/${entries.id}`;
 
-    return this.http.put(url, category).pipe(
+    return this.http.put(url, entries).pipe(
       catchError(this.handleError),
-      map(() => category)
+      map(() => entries)
     )
   }
 
@@ -53,25 +53,30 @@ export class CategoryService {
 
     return this.http.delete(url).pipe(
       map(() => null),
-      catchError(this.handleError)      
+      catchError(this.handleError)
     )
   }
 
 
   // PRIVATE METHODS
-  private jsonDataToCategories(jsondata: any[]): Category[] {
-    const categories: Category[] = [];
-    jsondata.forEach(element => categories.push(element as Category));
-    return categories;
+  private jsonDataToEntries(jsonData: any[]): Entry[] {
+    const entries: Entry[] = [];
+
+    jsonData.forEach(element => {
+      const entry = Object.assign(new Entry(), element);
+      entries.push(entry);
+    });
+
+    return entries;
+  }
+
+  private jsonDataToEntry(jsonData: any): Entry {
+    return Object.assign(new Entry(), jsonData);
   }
 
   private handleError(error: any): Observable<any> {
     console.error("ERRO NA REQUISIÇÃO => ", error);
     return throwError(error);
-  }
-
-  private jsonDataToCategory(jsondata: any): Category {
-    return jsondata as Category;
   }
 
 }
